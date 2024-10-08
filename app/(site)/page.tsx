93ea9dc2408b5b1e8a899e27339bea38b40110a3
dominickdevincenzo <devindm101@gmail.com>
2024-10-07 21:33:46 -0400

    completed auth UI with social signin

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -18,9 +18,10 @@ export default function Home() {
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <Image
           alt="Logo"
-          height="48"
-          width="48"
-          className="mx-auto w-auto"
+          height={64}
+          width={64}
+          className="mx-auto"
+          //   className="mx-auto w-16 h-16 sm:w-16 sm:h-16 lg:w-16 lg:h-16" // Adjust sizes based on screen size
           src="/images/logo.svg"
         />
         <h2
--- app/components/inputs/input.tsx
+++ app/components/inputs/input.tsx
@@ -22,21 +22,53 @@ const Input: React.FC<InputProps> = ({
   errors,
   disabled,
 }) => {
-  return;
-  <div>
-    <label
-      className="
+  return (
+    <div>
+      <label
+        className="
         block
         text-sm
         font-medium
         leading-6
         text-gray-900
         "
-      htmlFor={id}
-    >
-      {label}
-    </label>
-  </div>;
+        htmlFor={id}
+      >
+        {label}
+      </label>
+      <div className="mt-2">
+        <input
+          id={id}
+          type={type}
+          autoComplete={id}
+          disabled={disabled}
+          {...register(id, { required })}
+          className={clsx(
+            `
+            form-input
+            block
+            w-full
+            rounded-md
+            boarder-0
+            py-1.5
+            text-gray-900
+            shadow-sm
+            ring-1
+            ring-inset
+            ring-gray-300
+            placeholder: text-gray-400
+            focus: ring-2
+            focus: ring-inset
+            focus: ring-sky-600
+            sm: text-sm
+            sm: leading-6`,
+            errors[id] && "focus:ring-rose-500",
+            disabled && "opacity-50 cursor-default"
+          )}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Input;
