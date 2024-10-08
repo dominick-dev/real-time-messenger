7a095ad6a7b65a1db9c40d302bfa7c2492018b4e
dominickdevincenzo <devindm101@gmail.com>
2024-10-08 00:25:15 -0400

    added session to authform

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import ToasterContext from "./context/ToasterContext";
+import AuthContext from "./context/AuthContext";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,8 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ToasterContext />
-        {children}
+        <AuthContext>
+          <ToasterContext />
+          {children}
+        </AuthContext>
       </body>
     </html>
   );
