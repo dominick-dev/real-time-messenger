a196dd2e2659b1eeacb6be8f52130447621e8b9e
dominickdevincenzo <devindm101@gmail.com>
2024-10-08 17:58:02 -0400

    hard coded active bubble on user picture

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -22,8 +22,24 @@ const Avatar: React.FC<AvatarProps> = ({ user }) => {
         md:w-11
         "
       >
-        <Image alt="Avatar" src={user?.image || "/images/user.png"} fill />
+        <Image alt="Avatar" src={user?.image || "/images/profile.png"} fill />
       </div>
+      <span
+        className="
+            absolute
+            block
+            rounded-full
+            bg-green-500
+            ring-2
+            ring-white
+            top-0
+            right-0
+            h-2
+            w-2
+            md:h-3
+            md:w-3
+        "
+      />
     </div>
   );
 };
