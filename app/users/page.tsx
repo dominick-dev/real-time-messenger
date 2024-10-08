893c8d3ae0edd5f4fbe359d00c103982d43b34b3
dominickdevincenzo <devindm101@gmail.com>
2024-10-08 14:44:17 -0400

    created user logged in landing page

---
+++ app/users/page.tsx
@@ -0,0 +1,11 @@
+import EmptyState from "../components/EmptyState";
+
+const Users = () => {
+  return (
+    <div className="hidden lg:block lg:pl-8- h-full">
+      <EmptyState />
+    </div>
+  );
+};
+
+export default Users;
