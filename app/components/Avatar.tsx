23e4e79adfa3b348c0e295bae19d151c3c959ed6
dominickdevincenzo <devindm101@gmail.com>
2024-10-10 19:54:40 -0400

    sync active/inactive status across app

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -2,12 +2,16 @@
 
 import { User } from "@prisma/client";
 import Image from "next/image";
+import useActiveList from "../hooks/useActiveList";
 
 interface AvatarProps {
   user?: User;
 }
 
 const Avatar: React.FC<AvatarProps> = ({ user }) => {
+  const { members } = useActiveList();
+  const isActive = members.indexOf(user.email!) !== -1;
+
   return (
     <div className="relative">
       <div
@@ -24,22 +28,24 @@ const Avatar: React.FC<AvatarProps> = ({ user }) => {
       >
         <Image alt="Avatar" src={user?.image || "/images/profile.png"} fill />
       </div>
-      <span
-        className="
-            absolute
-            block
-            rounded-full
-            bg-green-500
-            ring-2
-            ring-white
-            top-0
-            right-0
-            h-2
-            w-2
-            md:h-2.5
-            md:w-2.5
-        "
-      />
+      {isActive && (
+        <span
+          className="
+              absolute
+              block
+              rounded-full
+              bg-green-500
+              ring-2
+              ring-white
+              top-0
+              right-0
+              h-2
+              w-2
+              md:h-2.5
+              md:w-2.5
+          "
+        />
+      )}
     </div>
   );
 };
