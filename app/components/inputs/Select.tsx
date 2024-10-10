b1651c314d0ae37ddae5673eecea19d217094637
dominickdevincenzo <devindm101@gmail.com>
2024-10-10 10:43:31 -0400

    added functionality to groupchat modal

--- app/components/inputs/Select.tsx
+++ app/components/inputs/Select.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import ReactSelect from "react-select";
+
 interface SelectProps {
   label: string;
   value?: Record<string, any>;
@@ -28,7 +30,25 @@ const Select: React.FC<SelectProps> = ({
       >
         {label}
       </label>
-      <div className="mt-2"></div>
+      <div className="mt-2">
+        <ReactSelect
+          isDisabled={disabled}
+          value={value}
+          onChange={onChange}
+          isMulti
+          options={options}
+          menuPortalTarget={document.body}
+          styles={{
+            menuPortal: (base) => ({
+              ...base,
+              zIndex: 999,
+            }),
+          }}
+          classNames={{
+            control: () => "text-sm",
+          }}
+        />
+      </div>
     </div>
   );
 };
